d966d1c5b9c6865c06064f166d2f481546ff7243
Adrián Rodríguez <Adrian.Rodriguez@indivasaas.com>
2024-06-22 16:14:18 +0200

    Actualizar yml 3 (videos y pantalla)

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: '5bxvgv',
+  video: true,
+  videosFolder: 'cypress/videos',
+  screenshotsFolder: 'cypress/screenshots',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
